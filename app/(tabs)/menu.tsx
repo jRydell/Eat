76359c8b3ad193335c4926bc634c31dcd1b48dca
Johan Rydell <johannilsrydell89@gmail.com>
2024-12-13 11:21:41 +0100

    menu fix

--- app/(tabs)/menu.tsx
+++ app/(tabs)/menu.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, Button, FlatList, StyleSheet } from "react-native";
-import { fetchRandomMeal } from "../../services/api";
-import { saveShoppingList, loadShoppingList } from "../../utils/storage";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
 import MenuGenerator from "@/components/MenuGenerator";
 
 export default function MenuTab() {
